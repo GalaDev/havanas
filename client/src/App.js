4b506a1d694c6ee9dada0f5ecf81a980b51e8536
Andy Gala <livelifebrands@gmail.com>
2020-07-28 18:36:10 -0700

    Add dummy data and render content

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import data from './data.js';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='grid-container'>
+      <header className='header'>
+        <div className='brand'>
+          <a href='index.html'>Havanas</a>
+        </div>
+        <div className='header-links'>
+          <div className='categories'>
+            <button className='dropbtn'>Categories</button>
+            <div className='categories-content'>
+              <li className='cigars'>
+                <a href='#'>Cigars</a>
+              </li>
+              <li className='lighters'>
+                <a href='#'>Lighters</a>
+              </li>
+              <li className='wrappers'>
+                <a href='#'>Wrappers</a>
+              </li>
+            </div>
+          </div>
+          <a href='signin.html'>Sign-In</a>
+          <a href='cart.html'>Cart</a>
+        </div>
       </header>
+      <main className='main'>
+        <h2>Featured Products</h2>
+        <div className='content'>
+          <ul className='products'>
+            {data.products.map(
+              (product) =>
+                product.rank < 5 && (
+                  <li>
+                    <div className='product'>
+                      <img
+                        className='product-image'
+                        src={product.image}
+                        alt={product.name}
+                      />
+                      <div className='product-name'>
+                        <a href='product.html'>{product.name}</a>
+                      </div>
+                      <div className='product-brand'>{product.brand}</div>
+                      <div className='product-price'>${product.price}</div>
+                    </div>
+                  </li>
+                )
+            )}
+          </ul>
+        </div>
+      </main>
+      <footer className='footer'>All rights reserved</footer>
     </div>
   );
 }
