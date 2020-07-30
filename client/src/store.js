d9a20cdcdf502dd3fcdb41bad2751259b429cfa1
Andy Gala <livelifebrands@gmail.com>
2020-07-29 17:27:32 -0700

    Add item count to state

--- client/src/store.js
+++ client/src/store.js
@@ -1,16 +1,20 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import { productListReducer } from './reducers/productReducers';
+import {
+  productListReducer,
+  productDetailsReducer,
+} from './reducers/productReducers';
 
 const initialState = {};
 const reducer = combineReducers({
   productList: productListReducer,
+  productDetails: productDetailsReducer,
 });
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const store = createStore(
   reducer,
   initialState,
-  compose(applyMiddleware(thunk))
+  composeEnhancer(applyMiddleware(thunk))
 );
 export default store;
